c4e3d88be99910e45026bf4cf20c46c3c78ebfe8
victorborges92 <vvb2@cin.ufpe.br>
2023-07-16 17:47:00 -0300

    Melhorei o design do front e fiz um loading

--- frontend/RPG-backstory-generator/src/App.jsx
+++ frontend/RPG-backstory-generator/src/App.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import { Container } from "react-bootstrap";
-
-
+import "./App.css";
 import CategoryForm from "./form";
-const app = () => {
- 
 
+const app = () => {
   return (
-    <Container>
-      <CategoryForm></CategoryForm>
-    </Container>
+    <div className="div-page">
+      <Container className="page-title">
+        <h3 className="mb-2">Seja bem vindo ao RPG Backstory Generator</h3>
+        <p>Escolha as características do seu personagem.</p>
+      </Container>
+      <Container>
+        <CategoryForm></CategoryForm>
+      </Container>
+    </div>
   );
 };
 
--- frontend/RPG-backstory-generator/src/form.jsx
+++ frontend/RPG-backstory-generator/src/form.jsx
@@ -1,197 +1,214 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import axios from 'axios';
+import axios from "axios";
+import "./form.css";
+import ReactLoading from 'react-loading';
+
 const CategoryForm = () => {
-    const [formData, setFormData] = useState({
-        height: "",
-        eyes: "",
-        hair_color: "",
-        hair_length: "",
-        hair_type: "",
-        body_type: "",
-        skin_tone: "",
-        facial_features: "",
-        clothing_style: "",
-        weapon_preference: "",
-        race: "",
-        character_class: "",
-        background: "",
-    });
-    const [toggle, setToggle] = useState(true)
-    const [history, setHistory] = useState("")
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
-    const instance = axios.create({
-        baseURL: 'http://localhost:8000'
-    });
+  const [formData, setFormData] = useState({
+    height: "",
+    eyes: "",
+    hair_color: "",
+    hair_length: "",
+    hair_type: "",
+    body_type: "",
+    skin_tone: "",
+    facial_features: "",
+    clothing_style: "",
+    weapon_preference: "",
+    race: "",
+    character_class: "",
+    background: "",
+  });
+  const [toggle, setToggle] = useState(true);
+  const [history, setHistory] = useState("");
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const instance = axios.create({
+    baseURL: "http://localhost:8000",
+  });
 
-    const fetchCharacther = async () => {
-        try {
-            const response = await instance.get('/character', {
-                params: formData,
-            });
-            console.log(response);
+  const fetchCharacther = async () => {
+    try {
+      const response = await instance.get("/character", {
+        params: formData,
+      });
+      console.log(response);
 
-            setHistory(response.data.content);
-            setToggle(false);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+      setHistory(response.data.content);
+      setToggle(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(formData);
-        fetchCharacther();
+  const [isLoading, setIsLoading] = useState(false);
 
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    fetchCharacther();
 
-    const categories = [
-        {
-            name: "height",
-            label: "Height",
-            options: ["Short", "Average", "Tall", "Very tall"],
-        },
-        {
-            name: "eyes",
-            label: "Eyes",
-            options: ["Blue", "Green", "Brown", "Gray", "Hazel"],
-        },
-        {
-            name: "hair_color",
-            label: "Color of hair",
-            options: ["Black", "Brown", "Blonde", "Red", "Gray", "White"],
-        },
-        {
-            name: "hair_length",
-            label: "Length of hair",
-            options: ["Bald", "Short", "Medium", "Long"],
-        },
-        {
-            name: "hair_type",
-            label: "Type of hair",
-            options: ["Straight", "Wavy", "Curly", "Braided", "Dreadlocks"],
-        },
-        {
-            name: "body_type",
-            label: "Body type",
-            options: ["Slim", "Athletic", "Muscular", "Curvy", "Stout", "Heavyset"],
-        },
-        {
-            name: "skin_tone",
-            label: "Skin tone",
-            options: ["Fair", "Light", "Medium", "Olive", "Dark", "Ebony"],
-        },
-        {
-            name: "facial_features",
-            label: "Facial features",
-            options: ["Clean-shaven", "Beard", "Mustache", "Sideburns", "Scars"],
-        },
-        {
-            name: "clothing_style",
-            label: "Clothing style",
-            options: [
-                "Elegant",
-                "Casual",
-                "Practical/Functional",
-                "Exotic",
-                "Formal",
-                "Vintage",
-            ],
-        },
-        {
-            name: "weapon_preference",
-            label: "Weapon preference",
-            options: ["Sword", "Bow", "Staff", "Dagger", "Hammer", "Shield"],
-        },
-        {
-            name: "race",
-            label: "Race",
-            options: [
-                "Human",
-                "Elf",
-                "Dwarf",
-                "Halfling",
-                "Tiefling",
-                "Dragonborn",
-                "Half-elf",
-                "Half-orc",
-                "Gnome",
-            ],
-        },
-        {
-            name: "character_class",
-            label: "Class",
-            options: [
-                "Fighter",
-                "Wizard",
-                "Rogue",
-                "Cleric",
-                "Bard",
-                "Paladin",
-                "Ranger",
-                "Sorcerer",
-                "Warlock",
-                "Monk",
-            ],
-        },
-        {
-            name: "background",
-            label: "Background",
-            options: [
-                "Noble",
-                "Outlander",
-                "Criminal",
-                "Acolyte",
-                "Sage",
-                "Soldier",
-                "Folk Hero",
-                "Urchin",
-                "Entertainer",
-                "Guild Artisan",
-            ],
-        },
-    ];
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+  };
 
-    return (
-        <>
+  const categories = [
+    {
+      name: "height",
+      label: "Height",
+      options: ["Short", "Average", "Tall", "Very tall"],
+    },
+    {
+      name: "eyes",
+      label: "Eyes",
+      options: ["Blue", "Green", "Brown", "Gray", "Hazel"],
+    },
+    {
+      name: "hair_color",
+      label: "Color of hair",
+      options: ["Black", "Brown", "Blonde", "Red", "Gray", "White"],
+    },
+    {
+      name: "hair_length",
+      label: "Length of hair",
+      options: ["Bald", "Short", "Medium", "Long"],
+    },
+    {
+      name: "hair_type",
+      label: "Type of hair",
+      options: ["Straight", "Wavy", "Curly", "Braided", "Dreadlocks"],
+    },
+    {
+      name: "body_type",
+      label: "Body type",
+      options: ["Slim", "Athletic", "Muscular", "Curvy", "Stout", "Heavyset"],
+    },
+    {
+      name: "skin_tone",
+      label: "Skin tone",
+      options: ["Fair", "Light", "Medium", "Olive", "Dark", "Ebony"],
+    },
+    {
+      name: "facial_features",
+      label: "Facial features",
+      options: ["Clean-shaven", "Beard", "Mustache", "Sideburns", "Scars"],
+    },
+    {
+      name: "clothing_style",
+      label: "Clothing style",
+      options: [
+        "Elegant",
+        "Casual",
+        "Practical/Functional",
+        "Exotic",
+        "Formal",
+        "Vintage",
+      ],
+    },
+    {
+      name: "weapon_preference",
+      label: "Weapon preference",
+      options: ["Sword", "Bow", "Staff", "Dagger", "Hammer", "Shield"],
+    },
+    {
+      name: "race",
+      label: "Race",
+      options: [
+        "Human",
+        "Elf",
+        "Dwarf",
+        "Halfling",
+        "Tiefling",
+        "Dragonborn",
+        "Half-elf",
+        "Half-orc",
+        "Gnome",
+      ],
+    },
+    {
+      name: "character_class",
+      label: "Class",
+      options: [
+        "Fighter",
+        "Wizard",
+        "Rogue",
+        "Cleric",
+        "Bard",
+        "Paladin",
+        "Ranger",
+        "Sorcerer",
+        "Warlock",
+        "Monk",
+      ],
+    },
+    {
+      name: "background",
+      label: "Background",
+      options: [
+        "Noble",
+        "Outlander",
+        "Criminal",
+        "Acolyte",
+        "Sage",
+        "Soldier",
+        "Folk Hero",
+        "Urchin",
+        "Entertainer",
+        "Guild Artisan",
+      ],
+    },
+  ];
 
-            {toggle && <Form>
-                {categories.map((category) => (
-                    <Form.Group key={category.name} className="mb-3">
-                        <Form.Label>{category.label}:</Form.Label>
-                        <div>
-                            {category.options.map((option) => (
-                                <Form.Check
-                                    key={option}
-                                    inline
-                                    required
-                                    type="radio"
-                                    name={category.name}
-                                    label={option}
-                                    value={option}
-                                    onChange={handleInputChange}
-                                />
-                            ))}
-                        </div>
-                    </Form.Group>
+  return (
+    <>
+      {toggle && (
+        <Form>
+          {categories.map((category) => (
+            <Form.Group key={category.name} className="mb-3 form-card">
+              <Form.Label className="card-title">{category.label}:</Form.Label>
+              <div>
+                {category.options.map((option) => (
+                  <Form.Check
+                    className="card-content"
+                    key={option}
+                    inline
+                    required
+                    type="radio"
+                    name={category.name}
+                    label={option}
+                    value={option}
+                    onChange={handleInputChange}
+                  />
                 ))}
+              </div>
+            </Form.Group>
+          ))}
 
-                <Button type="submit" onClick={handleSubmit}>
-                    Submit
-                </Button>
-            </Form>}
-            {!toggle &&
-                <p>{history}</p>
-            }
-        </>
-    );
+          <div className="div-btn">
+            <Button type="submit" onClick={handleSubmit}>
+              Submit
+            </Button>
+
+            {isLoading && (
+              <div className="div-loading">
+                <ReactLoading type="spin" color="#000" height={50} width={50} />
+              </div>
+            )}
+          </div>
+        </Form>
+      )}
+      {!toggle && <p>{history}</p>}
+    </>
+  );
 };
 
 export default CategoryForm;
